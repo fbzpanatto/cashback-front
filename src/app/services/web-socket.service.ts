f535b6aa942711eb3802ee00250c20c270069bcc
Fabrizio Panato <fbzpanatto@hotmail.com>
2025-03-25 16:34:42 -0300

    Starting Repo

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -1,5 +1,5 @@
-import { inject, Injectable } from '@angular/core';
-import { Socket } from 'socket.io-client';
+import { Injectable } from '@angular/core';
+import { io } from 'socket.io-client';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,29 +7,27 @@ import { Observable } from 'rxjs';
 })
 export class WebSocketService {
 
-  #socket = inject(Socket)
+  private socket = io('https://cashback-node.up.railway.app/')
 
   // Escuta o evento "qr" do WebSocket para receber o QR Code
   getQrCode(): Observable<string> {
     return new Observable(observer => {
-      this.#socket.on('qr', (qr: string) => {
-        console.log(qr)
-        observer.next(qr);
+      this.socket.on('qr', (qr: string) => {
+        observer.next(qr); // Envia o QR Code para quem estiver inscrito
       });
 
-      return () => this.#socket.off('qr'); // Remove o listener ao destruir a conexão
+      return () => this.socket.disconnect();
     });
   }
 
   // Escuta o evento "ready" para saber quando o WhatsApp está conectado
   getReadyStatus(): Observable<string> {
     return new Observable(observer => {
-      this.#socket.on('ready', (message: string) => {
-        console.log(message)
+      this.socket.on('ready', (message: string) => {
         observer.next(message);
       });
 
-      return () => this.#socket.off('ready');
+      return () => this.socket.disconnect();
     });
   }
 }
