cf48491cf838b265727af04ead065482ed54f039
Fabrizio Panato <fbzpanatto@hotmail.com>
2025-03-25 14:57:19 -0300

    Starting Repo

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const localpath = path.join(__dirname, '../../dist/cashback/browser')
+const fullPath = path.join(__dirname, '/browser')
 
 // Serve static files from Angular dist folder
-app.use(express.static(localpath))
+app.use(express.static(fullPath))
 
 // Redirect all requests to index.html (Angular routing)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(localpath, '/index.html'));
+  res.sendFile(path.join(fullPath, '/index.html'));
 });
 
 const PORT = 4200;
