e2dc1d3e41b389899c3ff4c4b913eb41bd3501fe
Fabrizio Panato <fbzpanatto@hotmail.com>
2025-03-27 08:56:20 -0300

    Starting Repo

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,7 @@
-// import express from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { createProxyMiddleware } from 'http-proxy-middleware';
-//
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-//
-// const app = express();
-// const PORT = parseInt(process.env['PORT'] || '4200');
-//
-// const API_URL = 'https://cashbacknode25.up.railway.app/';
-//
-// app.use('/api', createProxyMiddleware({
-//   target: API_URL,
-//   changeOrigin: true,
-//   pathRewrite: { '^/api': '' },
-//   logger: console,
-//   secure: true
-// }));
-//
-// const staticFilesPath = path.join(__dirname, 'browser');
-// app.use(express.static(staticFilesPath));
-//
-// app.get('/health', (req: Request, res: any) => res.sendStatus(200));
-//
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(staticFilesPath, 'index.html'));
-// });
-//
-// app.listen(PORT, () => {
-//   console.log(`🚀 Frontend rodando na porta: ${PORT}`);
-//   console.log(`🔗 Conectado à API: ${API_URL}`);
-//   console.log(`🌐 Acesse: https://cashback-front.up.railway.app`);
-// });
-
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -44,21 +9,27 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = parseInt(process.env['PORT'] || '4200');
 
-// Configuração do caminho dos arquivos estáticos
-const staticPath = path.join(__dirname, 'browser');
+const API_URL = 'https://app-cashback.up.railway.app/';
 
-// Middleware para arquivos estáticos
-app.use(express.static(staticPath));
+app.use('/api', createProxyMiddleware({
+  target: API_URL,
+  changeOrigin: true,
+  pathRewrite: { '^/api': '' },
+  logger: console,
+  secure: true
+}));
 
-// Health check para o Railway
-app.get('/health', async (req: Request, res: any) => res.sendStatus(200));
+const staticFilesPath = path.join(__dirname, 'browser');
+app.use(express.static(staticFilesPath));
 
-// Todas as rotas não encontradas vão para o Angular
+app.get('/health', (req: Request, res: any) => res.sendStatus(200));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(staticPath, 'index.html'));
+  res.sendFile(path.join(staticFilesPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
-  console.log(`📁 Servindo arquivos de: ${staticPath}`);
+  console.log(`🚀 Frontend rodando na porta: ${PORT}`);
+  console.log(`🔗 Conectado à API: ${API_URL}`);
+  console.log(`🌐 Acesse: https://cashback-front.up.railway.app`);
 });
