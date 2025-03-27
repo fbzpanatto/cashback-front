71f2255d9ec33b8a8616ac88d0ee5a212efc01ac
Fabrizio Panato <fbzpanatto@hotmail.com>
2025-03-27 08:07:02 -0300

    Starting Repo

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 export class WebSocketService {
 
   private socket = io('https://cashbacknode25.up.railway.app/')
+  // private socket = io('http://localhost:3333');
 
   // Escuta o evento "qr" do WebSocket para receber o QR Code
   getQrCode(): Observable<string> {
